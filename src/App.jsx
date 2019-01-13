b9b4797d439c2c73d18826f8deec751655b7455a
Hendrik <hendrik@reinvent.nl>
2019-01-13 12:30:28 +0100

    added Airbnb eslinting rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Column, Row } from './styled';
 
 class App extends Component {
   render() {
@@ -9,7 +10,11 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit
+            {' '}
+            <code>src/App.js</code>
+            {' '}
+and save to reload.
           </p>
           <a
             className="App-link"
---
+++ src/styled/index.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-content: center;
+  width: 800px;
+  justify-content: space-between;
+  margin: 0 auto;
+  @media (min-width: 320px) and (max-width: 780px) {
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    div {
+      order: 2;
+    }
+    section {
+      order: 1;
+    }
+  }
+`
